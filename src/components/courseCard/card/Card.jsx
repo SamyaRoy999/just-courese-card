e5507f8e5d96839c5e72ddb3999be0d5d745c3fb
samya-roy <samyaroy999@gmail.com>
2024-03-15 14:44:09 +0600

    daynamic data added

--- src/components/courseCard/card/Card.jsx
+++ src/components/courseCard/card/Card.jsx
@@ -1,15 +1,27 @@
+import { CiDollar } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
 
 const Card = (props) => {
-    const {title , price, description, credit} = props.CourseCard;
+    const { title, img, price, description, credit } = props.CourseCard;
     return (
         <div>
             <div className="card card-compact  bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={img} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <p>{description}</p>
+                    <div className="flex justify-between py-3">
+                        <div className="flex items-center">
+                            <CiDollar />
+                            <p>Price : {price}</p>
+                        </div>
+                        <div className="flex items-center"> 
+                            <CiBookmark />
+                            <p>credit : {credit}</p>
+                        </div>
+                    </div>
+                    <div className="card-actions w-full">
+                        <button className="btn w-full btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
